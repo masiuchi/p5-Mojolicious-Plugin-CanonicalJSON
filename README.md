158bf369f1da6c065a705ade0060722c318263c3
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2015-09-17 15:17:52 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Mojolicious::Plugin::CanonicalJSON - It's new $module
+
+# SYNOPSIS
+
+    use Mojolicious::Plugin::CanonicalJSON;
+
+# DESCRIPTION
+
+Mojolicious::Plugin::CanonicalJSON is ...
+
+# LICENSE
+
+Copyright (C) Masahiro Iuchi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Masahiro Iuchi &lt;masahiro.iuchi@gmail.com>
---
+++ lib/Mojolicious/Plugin/CanonicalJSON.pm
@@ -0,0 +1,39 @@
+package Mojolicious::Plugin::CanonicalJSON;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mojolicious::Plugin::CanonicalJSON - It's new $module
+
+=head1 SYNOPSIS
+
+    use Mojolicious::Plugin::CanonicalJSON;
+
+=head1 DESCRIPTION
+
+Mojolicious::Plugin::CanonicalJSON is ...
+
+=head1 LICENSE
+
+Copyright (C) Masahiro Iuchi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Masahiro Iuchi E<lt>masahiro.iuchi@gmail.comE<gt>
+
+=cut
+
